3a52acd7881056a4e5d115e567d1d71724f68981
Jean2505 <jean.victor12357@gmail.com>
2025-01-09 14:07:04 -0300

    User update

--- src/main/java/com/springproject/curso/repositories/OrderItemRepository.java
+++ src/main/java/com/springproject/curso/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.springproject.curso.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springproject.curso.entities.OrderItem;
+import com.springproject.curso.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
 
 }
